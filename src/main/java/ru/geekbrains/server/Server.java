e29b52b344fba266610de81b58f21cf61bb7606d
cotelCatherine <bezmozggoff@gmail.com>
2024-01-01 17:41:28 +0700

    server&client

---
+++ src/main/java/ru/geekbrains/server/Server.java
@@ -0,0 +1,60 @@
+package ru.geekbrains.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Server {
+
+    // message broker (kafka, redis, rabbitmq, ...)
+    // client sent letter to broker
+    // server sent to SMTP-server
+
+    public static final int PORT = 8181;
+
+    private static long clientIdCounter = 1L;
+    private static Map<Long, SocketWrapper> clients = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Сервер запущен на порту " + PORT);
+            while (true) {
+                final Socket client = server.accept();
+                final long clientId = clientIdCounter++;
+
+                SocketWrapper wrapper = new SocketWrapper(clientId, client);
+                System.out.println("Подключился новый клиент[" + wrapper + "]");
+                clients.put(clientId, wrapper);
+
+                new Thread(() -> {
+                    try (Scanner input = wrapper.getInput(); PrintWriter output = wrapper.getOutput()) {
+                        output.println("Подключение успешно. Список всех клиентов: " + clients);
+
+                        while (true) {
+                            String clientInput = input.nextLine();
+                            if (Objects.equals("q", clientInput)) {
+                                clients.remove(clientId);
+                                clients.values().forEach(it -> it.getOutput().println("Клиент[" + clientId + "] отключился"));
+                                break;
+                            }
+
+                            if (Objects.equals("@", clientInput.substring(0, 1))) {
+                                // формат сообщения: "цифра сообщение"
+                                long destinationId = Long.parseLong(clientInput.substring(1, 2));
+                                SocketWrapper destination = clients.get(destinationId);
+                                destination.getOutput().println(clientInput);
+                            } else {
+                                clients.values().forEach(it -> it.getOutput().println(clientInput));
+                            }
+                        }
+                    }
+                }).start();
+            }
+        }
+    }
+}
